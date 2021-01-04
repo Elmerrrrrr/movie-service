297dc9da61968a1f6dfecfb4a4fb6c93b8e443ce
Elmerrrrrr <volgers80@gmail.com>
2021-01-04 17:36:47 +0100

    Made a start with: https://start.spring.io/. Added the Spring Web Dependency and the OpenFeign dependency.

---
+++ NetflixAssignment/src/main/java/netflixclone/NetflixAssignment/NetflixAssignmentApplication.java
@@ -0,0 +1,13 @@
+package netflixclone.NetflixAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixAssignmentApplication.class, args);
+	}
+
+}
