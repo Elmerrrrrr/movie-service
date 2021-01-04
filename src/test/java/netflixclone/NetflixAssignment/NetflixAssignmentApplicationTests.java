21e0f8e14d1636ec37e72fb67d3b1e16047d5401
Elmerrrrrr <volgers80@gmail.com>
2021-01-04 20:59:46 +0100

    update

---
+++ src/test/java/netflixclone/NetflixAssignment/NetflixAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package netflixclone.NetflixAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
